78bcce67049de23ffb2993b01586cf8d01c44212
Tomasz Janisiewicz <tomekjanisiewi@gmail.com>
2026-07-22 14:13:01 +0200

    added ability to download data (demo includes 2026 year only)

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import time
+
+from api import get_data_format, get_voting
+from saver import get_file_name, save
+
+
+def download_json(year: int, month: int, term=10) -> None:
+    try:
+        date_from, date_to = get_data_format(year, month)
+        voting_data = get_voting(term, date_from, date_to)
+
+        file_name = get_file_name(year, month)
+        file_path = save(voting_data, file_name)
+    except ValueError as err0r:
+        print(f"Validation error: {year}-{month:02d}: {err0r}")
+    except RuntimeError as err0r:
+        print(f"SEJM API error: {year}-{month:02d}: {err0r}")
+
+def main():
+    dates_to_download = [(2026, 6),
+                         (2026, 5),
+                         (2026, 4),
+                         (2026, 3),
+                         (2026, 2),
+                         (2026, 1)]
+
+    term = 10
+
+    for year, month in dates_to_download:
+        download_json(year, month)
+        time.sleep(1)
+        print(f"succesfully downloaded {year}-{month:02d} data.")
+
+if __name__ == "__main__":
+    main()
